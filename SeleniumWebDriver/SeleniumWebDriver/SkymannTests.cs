d7b7799afc0006e284b3d07cc272f608b90e669e
Leonid Matveenko <trololosh41@gmail.com>
2019-11-05 13:06:32 +0300

    rename

--- SeleniumWebDriver/SeleniumWebDriver/SkymannTests.cs
+++ SeleniumWebDriver/SeleniumWebDriver/SkymannTests.cs
@@ -9,12 +9,12 @@ namespace SeleniumWebDriver
     [TestFixture]
     class SkymannTests
     {
-        private IWebDriver _webDriver;
+        private IWebDriver webDriver;
 
         [SetUp]
         public void StartBrowserChrome()
         {
-            _webDriver = new ChromeDriver
+            webDriver = new ChromeDriver
             {
                 Url = "https://skymann.com/waawo/?wurl=/orders_flights/enter_data/f84b9e1992/429f48260f3c13a487db348aa370bf817e4e033d"
             };
@@ -23,19 +23,19 @@ namespace SeleniumWebDriver
         [Test]
         public void EmptyField()
         {
-            WaitForElementToAppear(_webDriver, 15, By.XPath("//*[@id='waavoiframe0']"));
-            _webDriver.SwitchTo().Frame(_webDriver.FindElement(By.XPath("//*[@id='waavoiframe0']")));
+            WaitForElementToAppear(webDriver, 15, By.XPath("//*[@id='waavoiframe0']"));
+            webDriver.SwitchTo().Frame(webDriver.FindElement(By.XPath("//*[@id='waavoiframe0']")));
 
-            WaitForElementToAppear(_webDriver, 15, By.XPath("//*[@id='conditions']"));
-            var conditionsCheckBox = _webDriver.FindElement(By.XPath("//*[@id='conditions']"));
+            WaitForElementToAppear(webDriver, 15, By.XPath("//*[@id='conditions']"));
+            var conditionsCheckBox = webDriver.FindElement(By.XPath("//*[@id='conditions']"));
             conditionsCheckBox.Click();
 
-            WaitForElementToAppear(_webDriver, 15, By.XPath("//*[@id='OrdersFlightsEnterDataForm']/div[5]/div/div/div[2]/button"));
-            var submitButton = _webDriver.FindElement(By.XPath("//*[@id='OrdersFlightsEnterDataForm']/div[5]/div/div/div[2]/button"));
+            WaitForElementToAppear(webDriver, 15, By.XPath("//*[@id='OrdersFlightsEnterDataForm']/div[5]/div/div/div[2]/button"));
+            var submitButton = webDriver.FindElement(By.XPath("//*[@id='OrdersFlightsEnterDataForm']/div[5]/div/div/div[2]/button"));
             submitButton.Click();
 
-            WaitForElementToAppear(_webDriver, 15, By.XPath("//*[@id='OrdersFlightsEnterDataForm']/div[2]/div[2]/div[2]/div[1]/div/div"));
-            var errorMessage = _webDriver.FindElement(By.XPath("//*[@id='OrdersFlightsEnterDataForm']/div[2]/div[2]/div[2]/div[1]/div/div"));
+            WaitForElementToAppear(webDriver, 15, By.XPath("//*[@id='OrdersFlightsEnterDataForm']/div[2]/div[2]/div[2]/div[1]/div/div"));
+            var errorMessage = webDriver.FindElement(By.XPath("//*[@id='OrdersFlightsEnterDataForm']/div[2]/div[2]/div[2]/div[1]/div/div"));
             var isErrorMessageCorrect = errorMessage.Text.Equals("Введите имя, пожалуйста");
             Assert.IsTrue(isErrorMessageCorrect);
         }
@@ -43,21 +43,21 @@ namespace SeleniumWebDriver
         [Test]
         public void LongName()
         {
-            WaitForElementToAppear(_webDriver, 15, By.XPath("//*[@id='waavoiframe0']"));
-            _webDriver.SwitchTo().Frame(_webDriver.FindElement(By.XPath("//*[@id='waavoiframe0']")));
+            WaitForElementToAppear(webDriver, 15, By.XPath("//*[@id='waavoiframe0']"));
+            webDriver.SwitchTo().Frame(webDriver.FindElement(By.XPath("//*[@id='waavoiframe0']")));
 
-            WaitForElementToAppear(_webDriver, 15, By.XPath("//*[@id='conditions']"));
-            var conditionsCheckBox = _webDriver.FindElement(By.XPath("//*[@id='conditions']"));
+            WaitForElementToAppear(webDriver, 15, By.XPath("//*[@id='conditions']"));
+            var conditionsCheckBox = webDriver.FindElement(By.XPath("//*[@id='conditions']"));
             conditionsCheckBox.Click();
 
-            var surnameInput = _webDriver.FindElement(By.XPath("//*[@id='OrdersFlightsPassengers1Surname']"));
+            var surnameInput = webDriver.FindElement(By.XPath("//*[@id='OrdersFlightsPassengers1Surname']"));
             surnameInput.SendKeys("CHUPAKABRIK_CHIKIBRYAK_CHUPAKABRIK_CHIKIBRYAK_CHUPAKABRIK_CHIKIBRYAK_CHUPAKABRIK_CHIKIBRYAK");
 
-            var submitButton = _webDriver.FindElement(By.XPath("//*[@id='OrdersFlightsEnterDataForm']/div[5]/div/div/div[2]/button"));
+            var submitButton = webDriver.FindElement(By.XPath("//*[@id='OrdersFlightsEnterDataForm']/div[5]/div/div/div[2]/button"));
             submitButton.Click();
 
-            WaitForElementToAppear(_webDriver, 15, By.XPath("//*[@id='OrdersFlightsEnterDataForm']/div[2]/div[2]/div[2]/div[2]/div/div"));
-            var errorMessage = _webDriver.FindElement(By.XPath("//*[@id='OrdersFlightsEnterDataForm']/div[2]/div[2]/div[2]/div[2]/div/div"));
+            WaitForElementToAppear(webDriver, 15, By.XPath("//*[@id='OrdersFlightsEnterDataForm']/div[2]/div[2]/div[2]/div[2]/div/div"));
+            var errorMessage = webDriver.FindElement(By.XPath("//*[@id='OrdersFlightsEnterDataForm']/div[2]/div[2]/div[2]/div[2]/div/div"));
             var isErrorMessageCorrect = errorMessage.Text.Equals("Слишком длинное имя/фамилия. Свяжитесь с нами по телефону");
             Assert.IsTrue(isErrorMessageCorrect);
         }
@@ -71,7 +71,7 @@ namespace SeleniumWebDriver
         [TearDown]
         public void QuitDriver()
         {
-            _webDriver.Quit();
+            webDriver.Quit();
         }
     }
 }
