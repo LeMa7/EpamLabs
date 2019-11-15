63fd47080352d2488ac0a19e6650bc58ed51245f
Leonid Matveenko <trololosh41@gmail.com>
2019-11-15 09:17:25 +0300

    Add PageObjectsLab

---
+++ PageObject/PageObject/PageObjects/ReservationPage.cs
@@ -0,0 +1,50 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.PageObjects;
+using OpenQA.Selenium.Support.UI;
+using System;
+
+namespace PageObject.PageObjects
+{
+    public class ReservationPage
+    {
+        private readonly IWebDriver driver;
+
+        public IWebElement conditionsCheckBox => GetElement("//*[@id='conditions']");
+
+        public IWebElement nameInput => GetElement("//*[@id='OrdersFlightsPassengers1Name']");
+
+        public IWebElement submitButton => GetElement("//button[@class='btn btn-success btn-lg ladda-button']");
+
+        public ReservationPage(IWebDriver driver)
+        {
+            this.driver = driver;
+            PageFactory.InitElements(driver, this);
+            WaitForElementToAppear(driver, 15, By.XPath("//*[@id='waavoiframe0']"));
+            driver.SwitchTo().Frame(driver.FindElement(By.XPath("//*[@id='waavoiframe0']")));
+        }
+
+        public IWebElement GetElement(string xPath)
+        {
+            WaitForElementToAppear(driver, 15, By.XPath(xPath));
+
+            return driver.FindElement(By.XPath(xPath));
+        }
+
+        public static IWebElement WaitForElementToAppear(IWebDriver driver, int waitTime, By waitingElement)
+        {
+            IWebElement wait = new WebDriverWait(driver, TimeSpan.FromSeconds(waitTime)).Until(ExpectedConditions.ElementExists(waitingElement));
+            return wait;
+        }
+
+        public IAlert SwitchToAlert(IWebDriver driver)
+        {
+            IAlert alert = driver.SwitchTo().Alert();
+            return alert;
+        }
+
+        public IWebElement GetNameError()
+        {
+            return GetElement("//*[@id='OrdersFlightsEnterDataForm']/div[2]/div[2]/div[2]/div[1]/div/div");
+        }
+    }
+}
---
+++ PageObject/PageObject/Tests/SkymannTests.cs
@@ -0,0 +1,55 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using PageObject.PageObjects;
+
+namespace PageObject.Tests
+{
+    [TestFixture]
+    public class SkymannTests
+    {
+        private IWebDriver driver;
+
+        [SetUp]
+        public void StartBrowserChrome()
+        {
+            driver = new ChromeDriver
+            {
+                Url = "https://skymann.com/waawo/?wurl=/orders_flights/enter_data/f84b9e1992/429f48260f3c13a487db348aa370bf817e4e033d"
+            };
+        }
+
+        [Test]
+        public void RussianWordInNameInput()
+        {
+            var reservationPage = new ReservationPage(driver);
+            reservationPage.conditionsCheckBox.Click();
+
+            reservationPage.nameInput.SendKeys("Чикибряк");
+            reservationPage.submitButton.Click();
+
+            var isErrorMessageCorrect = reservationPage.GetNameError().Equals("Пожалуйста, введите правильное имя");
+            var a = reservationPage.GetNameError();
+            Assert.IsTrue(isErrorMessageCorrect);
+        }
+
+        [Test]
+        public void NoConditions()
+        {
+            var reservationPage = new ReservationPage(driver);
+
+            reservationPage.submitButton.Click();
+
+            IAlert alert = reservationPage.SwitchToAlert(driver);
+
+            var isErrorMessageCorrect = alert.Text.Equals("Примите наши условия, пожалуйста");
+            Assert.IsTrue(isErrorMessageCorrect);
+        }
+
+        [TearDown]
+        public void QuitDriver()
+        {
+            driver.Quit();
+        }
+    }
+}
