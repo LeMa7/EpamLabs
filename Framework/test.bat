3ddeabbfbf67a8d259780b7e56d5e2f6d272ccb2
Leonid Matveenko <trololosh41@gmail.com>
2020-01-08 23:39:04 +0300

    Add Wait for conditions checkbox

--- Framework/test.bat
+++ Framework/test.bat
@@ -1,4 +1,4 @@
 cd GitHubAutomation
 msbuild.exe Framework.csproj
 cd ../packages\NUnit.ConsoleRunner.3.10.0\tools
-nunit3-console.exe --testparam:browser=Chrome --testparam:environment=dev "../../../GitHubAutomation/bin/Debug/GitHubAutomation.dll" --where "cat=IncorrectEmail"
+nunit3-console.exe --testparam:browser=Chrome --testparam:environment=dev "../../../GitHubAutomation/bin/Debug/GitHubAutomation.dll" --where "cat=DontChoose"
